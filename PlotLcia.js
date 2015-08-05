7e2ef8b7f51215187754641892d461d747559d31
Sabina Beraha <binaworks@verizon.net>
2015-08-05 14:55:22 -0700

    add a table row for every method

--- PlotLcia.js
+++ PlotLcia.js
@@ -1,50 +1,91 @@
 function PlotLcia() {
-    var colorScale = new Plottable.Scales.Color();
     var xScale = new Plottable.Scales.Category();
-    var yScale = new Plottable.Scales.Linear();
-    var xAxis = new Plottable.Axes.Category(xScale, "bottom");
-    var yAxis = new Plottable.Axes.Numeric(yScale, "left");
-    var plot = new Plottable.Plots.Bar()
-        .x(function(d) { return d.lciaScore[0].processID.toString(); }, xScale)
-        .y(function(d) { return d["total"]; }, yScale);
-    var dataSet = null;
-
-
-    var chart = new Plottable.Components.Table([
-        [yAxis, plot],
-        [null, xAxis]
-    ]);
-
-
-    function loadData() {
-        var urlPath = "http://kbcalr.isber.ucsb.edu/api/scenarios/1/processes/",
-            processID = ["115", "150"],
-            url = urlPath + processID[0] + "/lciaresults";
-
-        d3.json(url, function(error, json) {
-            if (error) return console.warn(error);
-            dataSet = json;
-            url = urlPath + processID[1] + "/lciaresults";
-            d3.json(url, function(error, json) {
-                if (error) return console.warn(error);
-                dataSet = dataSet.concat(json);
-                plotData();
-            });
-        });
-    }
+    var lciaResults = [];
+    var lciaMethods = [];
+    var scenarios = [];
+    var processes = [];
+    var tableComponents = [];
 
     loadData();
 
-    function plotData() {
-        var lciaMethodID = 1,
-            mds = dataSet.filter( function(d) {
-                return d["lciaMethodID"] === lciaMethodID;
+    function displayResults(results) {
+        lciaResults = d3.merge(results);
+        plotData()
+    }
+
+    function loadResults() {
+
+        queue()
+            .defer(d3.json,"http://kbcalr.isber.ucsb.edu/api/scenarios/1/processes/115/lciaresults")
+            .defer(d3.json,"http://kbcalr.isber.ucsb.edu/api/scenarios/1/processes/150/lciaresults")
+            .awaitAll(function(error, results) {
+                if (error) return console.error(error);
+                displayResults(results);
             });
+    }
 
-        yAxis.formatter(d3.format("^.2g"));
-        plot.addDataset(new Plottable.Dataset(mds))
-            ;
+    function loadData() {
+        queue()
+            .defer(d3.json,"http://kbcalr.isber.ucsb.edu/api/lciamethods")
+            .defer(d3.json,"http://kbcalr.isber.ucsb.edu/api/processes")
+            .defer(d3.json,"http://kbcalr.isber.ucsb.edu/api/scenarios")
+            .await(function(error, m, p, s) {
+                if (error) return console.error(error);
+                lciaMethods = m;
+                processes = p;
+                scenarios = s;
+                loadResults();
+            });
+    }
 
+    function getProcessCategory(r) {
+        return r.lciaScore[0].processID.toString();
+    }
+
+    function getResult(r) {
+        return r["total"];
+    }
+
+    function getDomain(ds) {
+        var extent = [0,0];
+        ds.forEach( function(r) {
+            var val = getResult(r);
+            if (val < extent[0]) {
+                extent[0] = val;
+            } else if (val > extent[1]) {
+                extent[1] = val;
+            }
+        });
+        return extent;
+    }
+
+    function plotMethodResults(m) {
+        var xAxis = new Plottable.Axes.Category(xScale, "bottom");
+        var yScale = new Plottable.Scales.Linear();
+        var yAxis = new Plottable.Axes.Numeric(yScale, "left").formatter(d3.format("^.2g"));
+        var ds =  lciaResults.filter( function(d) {
+            return d["lciaMethodID"] === m.lciaMethodID;
+        });
+        var plot = new Plottable.Plots.Bar();
+        //var domain = getDomain(ds);
+
+        //yScale.domain(domain);
+        plot.x(getProcessCategory, xScale)
+            .y(getResult, yScale);
+
+        plot.addDataset(new Plottable.Dataset(ds));
+        tableComponents.push([yAxis, plot],
+                             [null, xAxis]);
+    }
+
+    function plotData() {
+        var chart;
+
+        lciaMethods.forEach(plotMethodResults);
+        chart = new Plottable.Components.Table( tableComponents);
+        d3.select("#chart")
+            .attr("height", lciaMethods.length * 200)
+            .attr("width", 200);
         chart.renderTo("#chart");
     }
 
